c9896444d81754bd3e0f5144003b6dad368423c9
Chante Bethell <chantebethell@gmail.com>
2021-10-05 10:04:24 -0500

    implement code review changes
    - flesh out docs
    - fix command line flags 
    - remove dimension reduction steps

--- 02-normalize-sce.R
+++ 02-normalize-sce.R
@@ -1,11 +1,13 @@
-## Read in and normalize filtered SingleCellExperiment data
+## Read in SingleCellExperiment RDS object that has been filtered for highly
+## expressed cells, using miQC or a manual filtering method, and filtered for
+## highly expressed genes. This script will then normalize the filtered object
+## using functions from the `scran` and `scater` packages.
 
 # Command line usage:
 
 # Rscript --vanilla 02-normalize-sce.R \
-#   --sample_sce_filepath "data/anderson-single-cell/filtered/filtered_GSM4186961_miQC_sce.rds" \
-#   --sample_name "GSM4186961" \
-#   --output_data_directory data/anderson-single-cell/normalized \
+#   --sce "data/anderson-single-cell/filtered/filtered_GSM4186961_miQC_sce.rds" \
+#   --output_filepath data/anderson-single-cell/normalized/normalized_GSM4186961_sce.rds \
 #   --seed 2021 \
 
 ## Set up -------------------------------------------------------------
@@ -21,22 +23,16 @@ library(optparse)
 # Declare command line options
 option_list <- list(
   optparse::make_option(
-    c("-a", "--filtered_sample_sce_filepath"),
+    c("-i", "--sce"),
     type = "character",
     default = NULL,
     help = "path to filtered sample SCE RDS file",
   ),
   optparse::make_option(
-    c("-r", "--sample_name"),
+    c("-o", "--output_filepath"),
     type = "character",
     default = NULL,
-    help = "name of sample being analyzed",
-  ),
-  optparse::make_option(
-    c("-o", "--output_data_directory"),
-    type = "character",
-    default = NULL,
-    help = "output normalized data directory"
+    help = "path to output normalized RDS file"
   ),
   optparse::make_option(
     c("-s", "--seed"),
@@ -54,21 +50,18 @@ opt <- optparse::parse_args(opt_parser)
 # Set the seed
 set.seed(opt$seed)
 
-## Define file paths
-
 # Directory and file to save output
-normalized_dir <- file.path(opt$output_data_directory)
-if (!dir.exists(normalized_dir)) {
-  dir.create(normalized_dir, recursive = TRUE)
-}
+output_file <- opt$output_filepath
+output_dir <- dirname(output_file)
 
-output_sce_file <- file.path(
-  normalized_dir, paste0("normalized_", opt$sample_name, "_sce.rds"))
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
 
 #### Read in data --------------------------------------------------------------
 
 # Read in filtered sce object
-filtered_sce <- readr::read_rds(opt$filtered_sample_sce_filepath)
+filtered_sce <- readr::read_rds(opt$sce)
 
 #### Normalize the data --------------------------------------------------------
 
@@ -81,22 +74,6 @@ filtered_sce <- scran::computeSumFactors(filtered_sce, clusters = qclust)
 # Normalize and log transform
 normalized_sce <- scater::logNormCounts(filtered_sce)
 
-#### Add PCA and UMAP results --------------------------------------------------
+#### Save output normalized file -----------------------------------------------
 
-# Model gene variance using `scran:modelGeneVar()`
-gene_variance <- scran::modelGeneVar(normalized_sce)
-
-# Select the most variable genes
-subset_genes <- scran::getTopHVGs(gene_variance, n = 2000)
-
-# Calculate a PCA matrix using those genes
-normalized_sce <-
-  runPCA(normalized_sce, subset_row = subset_genes)
-
-# Calculate a UMAP matrix using the PCA results
-normalized_sce <- runUMAP(normalized_sce,
-                          dimred = "PCA")
-
-#### Save output normalized file with dimensionality results -------------------
-
-readr::write_rds(normalized_sce, output_sce_file)
+readr::write_rds(normalized_sce, output_file)

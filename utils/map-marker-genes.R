f0587dbe3c1993e5b35b9d4549f46cf785b10695
Chante Bethell <chantebethell@gmail.com>
2021-10-06 13:22:01 -0500

    incorporate code review refactoring suggestions
    - add in additional command line options for flexibility and re-run for testing

--- utils/map-marker-genes.R
+++ utils/map-marker-genes.R
@@ -4,36 +4,68 @@
 
 # Command line usage:
 # Rscript --vanilla 00-map-marker-genes.R \
-#         --input_marker_gene_list "all_marker_genes.tsv" \
-#         --type_of_identifier "gene symbol" \
-#         --output_file "mapped_all_marker_genes.tsv"
+# --input_marker_gene_list "data/anderson-single-cell/marker-genes/nb_marker_genes.tsv" \
+# --input_identifiers "SYMBOL" \
+# --output_identifiers "ENSEMBL" \
+# --identifier_column_name "gene_symbol" \
+# --organism "human" \
+# --multi_mappings "list" \
+# --output_file "data/anderson-single-cell/marker-genes/mapped_nb_marker_genes.tsv"
 
 #### Load libraries ------------------------------------------------------------
 
 library(magrittr)
 library(readr)
 library(AnnotationDbi)
-library(org.Hs.eg.db)
 
 #### Command line arguments/options --------------------------------------------
 
 # Declare command line options
 option_list <- list(
   optparse::make_option(
-    c("-i", "--input_marker_gene_list"),
+    c("-l", "--input_marker_gene_list"),
     type = "character",
     default = NULL,
     help = "file path to the unmapped input marker gene list(s)"
   ),
   optparse::make_option(
-    c("-t", "--type_of_identifier"),
+    c("-i", "--input_identifiers"),
     type = "character",
     default = NULL,
-    help = "type of gene identifiers provided in the list(s) -- can be gene
-            symbol or ensembl gene id"
+    help = "type of gene identifiers provided in the list(s) -- 'SYMBOL' or
+           'ENSEMBL' for example, see `keytypes(org.Xx.eg.db)` for more options"
   ),
   optparse::make_option(
-    c("-o", "--output_file"),
+    c("-e", "--output_identifiers"),
+    type = "character",
+    default = NULL,
+    help = "type of gene identifiers to be mapped to and returned-- see
+            `keytypes(org.Xx.eg.db)` for more options"
+  ),
+  optparse::make_option(
+    c("-c", "--identifier_column_name"),
+    type = "character",
+    default = NULL,
+    help = "the name of the column in the input file that contains the gene
+            identifiers to be converted from"
+  ),
+  optparse::make_option(
+    c("-o", "--organism"),
+    type = "character",
+    default = NULL,
+    help = "the name of the organism annotation Bioconductor package relevant to
+            the genes to be mapped"
+  ),
+  optparse::make_option(
+    c("-m", "--multi_mappings"),
+    type = "character",
+    default = NULL,
+    help = "how should instances of multiple gene identifier mappings be handled
+            - may specify 'list' to return all the mappings or 'first' to keep
+            only the first mapping"
+  ),
+  optparse::make_option(
+    c("-p", "--output_file"),
     type = "character",
     default = "all_marker_genes_mapped.tsv",
     help = "file path to the mapped output marker gene file"
@@ -51,61 +83,53 @@ output_dir <- dirname(output_file)
 if (!dir.exists(output_dir)) {
   dir.create(output_dir, recursive = TRUE)
 }
+
 #### Read in data --------------------------------------------------------------
 
-marker_genes <- data.table::fread(opt$input_marker_gene_list)
+marker_genes <- data.table::fread(opt$input_marker_gene_list, stringsAsFactors = FALSE)
 
 #### Perform mapping -----------------------------------------------------------
 
-if (opt$type_of_identifier == "gene symbol") {
-  marker_genes_mapped <- mapIds(
-    # organism annotation package
-    org.Hs.eg.db,
-    # the provided gene identifiers
-    keys = marker_genes$gene_symbol,
-    # the type of provided gene identifiers
-    keytype = "SYMBOL",
-    # the type of gene identifiers to map to
-    column = "ENSEMBL",
-    multiVals = "list"
-  ) %>%
-    tibble::enframe(name = "gene_symbol", value = "gene_id") %>%
-    # enframe() makes a `list` column; we will simplify it with unnest()
-    # This will result in one row of our data frame per list item
-    tidyr::unnest(cols = gene_id) %>%
-    # grab only the unique rows
-    dplyr::distinct() %>%
-    # join the remaining columns
-    dplyr::left_join(marker_genes, by = "gene_symbol")
-} else if (opt$type_of_identifier == "ensembl id") {
-  marker_genes_mapped <- mapIds(
-    # organism annotation package
-    org.Hs.eg.db,
-    # the provided gene identifiers
-    keys = marker_genes$gene_id,
-    # the type of provided gene identifiers
-    keytype = "ENSEMBL",
-    # the type of gene identifiers to map to
-    column = "SYMBOL",
-    multiVals = "list"
-  ) %>%
-    tibble::enframe(name = "gene_id", value = "gene_symbol") %>%
-    # enframe() makes a `list` column; we will simplify it with unnest()
-    # This will result in one row of our data frame per list item
-    tidyr::unnest(cols = gene_symbol) %>%
-    # grab only the unique rows
-    dplyr::distinct() %>%
-    # join the remaining columns
-    dplyr::left_join(marker_genes, by = "gene_id")
-} else {
-  stop(
-    "Incorrect input gene identifier columns. Columns containing gene symbols
-       should be named `gene_symbol` while those containing Ensembl gene IDs
-       should be named `gene_id`"
-  )
+# turn column name into symbol for pulling the column info out of data frame
+identifier_column_name <- rlang::sym(opt$identifier_column_name)
+ids_for_mapping <- marker_genes %>%
+  dplyr::pull(identifier_column_name)
+
+# define the annotation packages based on the specified organism
+if(opt$organism == "human") {
+  library(org.Hs.eg.db)
+  annotation_package <- org.Hs.eg.db
+} else if (opt$organism == "mouse") {
+  library(org.Mm.eg.db)
+  annotation_package <- org.Mm.eg.db
+} else if (opt$organism == "zebrafish") {
+  library(org.Dr.eg.db)
+  annotation_package <- org.Dr.eg.db
 }
+
+marker_genes_mapped <- mapIds(
+  # organism annotation package
+  annotation_package,
+  # the provided gene identifiers
+  keys = ids_for_mapping,
+  # the type of provided gene identifiers
+  keytype = opt$input_identifiers,
+  # the type of gene identifiers to map to
+  column = opt$output_identifiers,
+  multiVals = opt$multi_mappings
+) %>%
+  tibble::enframe(
+    name = opt$identifier_column_name,
+    value = tolower(opt$output_identifiers)
+  ) %>%
+  # enframe() makes a `list` column; we will simplify it with unnest()
+  # This will result in one row of our data frame per list item
+  tidyr::unnest(cols = tolower(opt$output_identifiers)) %>%
+  # grab only the unique rows
+  dplyr::distinct() %>%
+  # join the remaining columns
+  dplyr::left_join(marker_genes, by = opt$identifier_column_name)
 
 #### Save mapped object to file ------------------------------------------------
 
 write_tsv(marker_genes_mapped, opt$output_file)
-

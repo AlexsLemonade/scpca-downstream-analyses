c1437df57bbdabb1c805a8bd3e4fa8cb9fa6138e
Chante Bethell <chantebethell@gmail.com>
2021-10-07 09:08:46 -0500

    incorporate review refactoring suggestions and doc updates

--- utils/map-marker-genes.R
+++ utils/map-marker-genes.R
@@ -1,24 +1,21 @@
 # Map provided gene identifiers for downstream use in the marker genes analysis.
-# The format of and the column in which the provided gene identifiers should be
-# specified using the appropriate command line flags outline below, for mapping
-# to the desired gene identifiers as should also be specifed via a command line
-# flag.
+# The format of the provided gene identifiers, and the and the column in which
+# they can be found in the input file, should be specified using the appropriate
+# command line flags outlined below. The desired gene identifiers to be mapped
+# to should also be specified via the appropriate command line flag.
+# 
+# Run `Rscript utils/map-marker-genes.R --help` for more on what values should
+# be specified for each of the command line flags.
 
 # Command line usage:
-# Rscript --vanilla 00-map-marker-genes.R \
+# Rscript --vanilla utils/map-marker-genes.R \
 # --input_marker_gene_list "data/anderson-single-cell/marker-genes/nb_marker_genes.tsv" \
 # --input_identifiers "SYMBOL" \
 # --output_identifiers "ENSEMBL" \
 # --identifier_column_name "gene_symbol" \
-# --organism "human" \
+# --organism "Homo sapiens" \
 # --multi_mappings "list" \
 # --output_file "data/anderson-single-cell/marker-genes/mapped_nb_marker_genes.tsv"
-
-#### Load libraries ------------------------------------------------------------
-
-library(magrittr)
-library(readr)
-library(AnnotationDbi)
 
 #### Command line arguments/options --------------------------------------------
 
@@ -28,21 +25,21 @@ option_list <- list(
     c("-l", "--input_marker_gene_list"),
     type = "character",
     default = NULL,
-    help = "file path to the unmapped input marker gene list(s)"
+    help = "file path to the unmapped input marker gene list"
   ),
   optparse::make_option(
     c("-i", "--input_identifiers"),
     type = "character",
     default = NULL,
-    help = "type of gene identifiers provided in the list(s) -- 'SYMBOL' or
-           'ENSEMBL' for example, see `keytypes(org.Xx.eg.db)` for more options"
+    help = "type of gene identifiers provided in the list -- 'SYMBOL' or
+           'ENSEMBL' for example, see `keytypes()` in the organism's annotation
+            package for more options"
   ),
   optparse::make_option(
     c("-e", "--output_identifiers"),
     type = "character",
     default = NULL,
-    help = "type of gene identifiers to be mapped to and returned-- see
-            `keytypes(org.Xx.eg.db)` for more options"
+    help = "type of gene identifiers to be mapped to and returned"
   ),
   optparse::make_option(
     c("-c", "--identifier_column_name"),
@@ -55,8 +52,8 @@ option_list <- list(
     c("-o", "--organism"),
     type = "character",
     default = NULL,
-    help = "the name of the organism annotation Bioconductor package relevant to
-            the genes to be mapped"
+    help = "the scientific name of the organism relevant to the genes to be
+            mapped, 'Homo sapiens' or 'Mus musculus' for examples"
   ),
   optparse::make_option(
     c("-m", "--multi_mappings"),
@@ -69,7 +66,7 @@ option_list <- list(
   optparse::make_option(
     c("-p", "--output_file"),
     type = "character",
-    default = "all_marker_genes_mapped.tsv",
+    default = NULL,
     help = "file path to the mapped output marker gene file"
   )
 )
@@ -86,6 +83,12 @@ if (!dir.exists(output_dir)) {
   dir.create(output_dir, recursive = TRUE)
 }
 
+#### Load libraries ------------------------------------------------------------
+
+library(magrittr)
+library(readr)
+library(AnnotationDbi)
+
 #### Read in data --------------------------------------------------------------
 
 marker_genes <- data.table::fread(opt$input_marker_gene_list, stringsAsFactors = FALSE)
@@ -97,18 +100,22 @@ identifier_column_name <- rlang::sym(opt$identifier_column_name)
 ids_for_mapping <- marker_genes %>%
   dplyr::pull(identifier_column_name)
 
-# define the annotation packages based on the specified organism
-if(opt$organism == "human") {
-  library(org.Hs.eg.db)
-  annotation_package <- org.Hs.eg.db
-} else if (opt$organism == "mouse") {
-  library(org.Mm.eg.db)
-  annotation_package <- org.Mm.eg.db
-} else if (opt$organism == "zebrafish") {
-  library(org.Dr.eg.db)
-  annotation_package <- org.Dr.eg.db
+# Define the annotation packages based on the specified organism
+annotation_list <- list(
+  'Homo sapiens' = "org.Hs.eg.db",
+  'Mus musculus' = "org.Mm.eg.db",
+  'Danio rerio' = "org.Dr.eg.db"  #,
+  # new organisms would go here
+)
+# Error handling
+if (!(opt$organism %in% names(annotation_list))){
+  stop(paste(opt$organism, "is not supported"))
 }
+# Load library and assign annotation package
+library(annotation_list[[opt$organism]], character.only = TRUE)
+annotation_package <- eval(parse(text = annotation_list[[opt$organism]]))
 
+# Perform mapping
 marker_genes_mapped <- mapIds(
   # organism annotation package
   annotation_package,

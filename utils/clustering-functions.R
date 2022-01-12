4fff13abafb8e8a231a39c21072d7d57db0e188f
Chante Bethell <chantebethell@gmail.com>
2022-01-12 09:54:49 -0600

    propagate refactoring and docs changes to individual functions

--- utils/clustering-functions.R
+++ utils/clustering-functions.R
@@ -5,8 +5,19 @@
 #
 # source(file.path("utils", "clustering-functions.R"))
 
-perform_clustering <- function(normalized_sce, k, cluster_name, check_stability = FALSE, seed = 2021) {
-  # Purpose: Perform the k-means clustering on a normalized SingleCellExperiment object
+kmeans_clustering <- function(normalized_sce, k, cluster_name,
+                              check_stability = FALSE, seed = 2021) {
+  # Purpose: Perform the k-means clustering on a normalized SingleCellExperiment
+  # object
+  
+  # Args:
+  #   normalized_sce: normalized SingleCellExperiment object
+  #   k: the desired number of centers
+  #   cluster_name: name for storing the cluster results in the
+  #                 SingleCellExperiment object
+  #   check_stability: if 'TRUE', the stability of the clusters will be
+  #                    calculated; the default is 'FALSE'
+  #   seed: an integer to set the seed as for reproducibility
   
   # set the seed for reproducible results
   set.seed(seed)
@@ -14,64 +25,138 @@ perform_clustering <- function(normalized_sce, k, cluster_name, check_stability 
   # extract the principal components matrix
   pca_matrix <- reducedDim(normalized_sce, "PCA")
   
-  if (startsWith(cluster_name, "kcluster")) {
-    # perform k-means clustering
-    clusters <- clusterRows(pca_matrix, KmeansParam(centers = k))
-  } else if (startsWith(cluster_name, "walktrap_cluster")) {
-    clusters <- clusterRows(pca_matrix, NNGraphParam(k = k,
-                                                     cluster.fun = "walktrap"))
-  } else if (startsWith(cluster_name, "louvain_cluster")) {
-    clusters <- clusterRows(pca_matrix,
-                            NNGraphParam(
-                              k = k,
-                              type = "jaccard",
-                              cluster.fun = "louvain"
-                            ))
-  }
+  # perform k-means clustering
+  clusters <- clusterRows(pca_matrix, KmeansParam(centers = k))
   
   # store cluster results in the SCE object
   normalized_sce[[cluster_name]] <- factor(clusters)
   
   # check cluster stability if the `check_stability == TRUE`
   if (check_stability) {
-    if (startsWith(cluster_name, "kcluster")) {
-      # Create subfunctions for `bootstrapStability()`
-      cluster_stability_subfunction <- function(x) {
-        clusterRows(x, KmeansParam(centers = k))
-      }
-    } else if (startsWith(cluster_name, "walktrap_cluster")) {
-      cluster_stability_subfunction <- function(x) {
-        clusterRows(x, NNGraphParam(k = k, cluster.fun = "walktrap"))
-      }
-    } else if (startsWith(cluster_name, "louvain_cluster")) {
-      cluster_stability_subfunction <- function(x) {
-        clusterRows(x,
-                    NNGraphParam(
-                      k = k,
-                      type = "jaccard",
-                      cluster.fun = "louvain"
-                    ))
-      }
-    }
-    
     # create name for the bootstrapping results to be stored in
     bootstrapping_name <-
       paste("bootstrapping_results", cluster_name, sep = "_")
     
-    # run the `bootstapStability()` function
     metadata(normalized_sce)[[bootstrapping_name]] <-
-      bootstrapStability(pca_matrix,
-                         FUN = cluster_stability_subfunction,
-                         clusters = normalized_sce[[cluster_name]])
-    
+      check_cluster_stability(normalized_sce,
+                              pca_matrix,
+                              bootstrapping_name,
+                              cluster_name,
+                              k)
   }
   
   return(normalized_sce)
   
 }
 
+graph_clustering <- function(normalized_sce, k, cluster_name, type,
+                             cluster_function, check_stability = FALSE, seed = 2021) {
+  # Purpose: Perform the k-means clustering on a normalized SingleCellExperiment
+  # object
+  
+  # Args:
+  #   normalized_sce: normalized SingleCellExperiment object
+  #   k: the desired number of centers
+  #   cluster_name: name for storing the cluster results in the
+  #                 SingleCellExperiment object
+  #   type: the type of weighting scheme -- can by "jaccard" or "rank"
+  #   cluster_function: the name of the community detection algorithm that is
+  #                     being tested -- can be "walktrap" or "louvain"
+  #   check_stability: if 'TRUE', the stability of the clusters will be
+  #                    calculated; the default is 'FALSE'
+  #   seed: an integer to set the seed as for reproducibility
+  
+  
+  # set the seed for reproducible results
+  set.seed(seed)
+  
+  # turn arguments into symbols
+  type_sym <- rlang::sym(type)
+  cluster_function_sym <- rlang::sym(cluster_function)
+  
+  # extract the principal components matrix
+  pca_matrix <- reducedDim(normalized_sce, "PCA")
+  
+  # perform graph-based clustering
+  clusters <- clusterRows(pca_matrix,
+                          NNGraphParam(
+                            k = k,
+                            type = type,
+                            cluster.fun = cluster_function
+                          ))
+  
+  # store cluster results in the SCE object
+  normalized_sce[[cluster_name]] <- factor(clusters)
+  
+  # check cluster stability if the `check_stability == TRUE`
+  if (check_stability) {
+    # create name for the bootstrapping results to be stored in
+    bootstrapping_name <-
+      paste("bootstrapping_results", cluster_name, sep = "_")
+    
+    metadata(normalized_sce)[[bootstrapping_name]] <-
+      check_cluster_stability(normalized_sce,
+                              pca_matrix,
+                              bootstrapping_name,
+                              cluster_name,
+                              k)
+  }
+  
+  return(normalized_sce)
+  
+}
+
+check_cluster_stability <- function(normalized_sce, pca_matrix,
+                                    bootstrapping_name, cluster_name, k) {
+  # Purpose: To use bootstrapping to check the stability of given clusters
+  
+  # Args:
+  #   normalized_sce: normalized SingleCellExperiment object
+  #   pca_matrix: the matrix object that contains the PCA results that were
+  #               extracted from the noramlized SingleCellExperiment object
+  #   bootstrapping_name: name for storing the bootstrapping results in the
+  #                       SingleCellExperiment object
+  #   cluster_name: name associated with where the cluster results in the
+  #                 SingleCellExperiment object are stored
+  #   k: the desired number of centers
+  
+  # check cluster stability if the `check_stability == TRUE`
+  if (startsWith(cluster_name, "kcluster")) {
+    # Create subfunctions for `bootstrapStability()`
+    cluster_stability_subfunction <- function(x) {
+      clusterRows(x, KmeansParam(centers = k))
+    }
+  } else if (startsWith(cluster_name, "walktrap_cluster")) {
+    cluster_stability_subfunction <- function(x) {
+      clusterRows(x, NNGraphParam(k = k, cluster.fun = "walktrap"))
+    }
+  } else if (startsWith(cluster_name, "louvain_cluster")) {
+    cluster_stability_subfunction <- function(x) {
+      clusterRows(x,
+                  NNGraphParam(
+                    k = k,
+                    type = "jaccard",
+                    cluster.fun = "louvain"
+                  ))
+    }
+  }
+  
+  # run the `bootstapStability()` function
+  metadata(normalized_sce)[[bootstrapping_name]] <-
+    bootstrapStability(pca_matrix,
+                       FUN = cluster_stability_subfunction,
+                       clusters = normalized_sce[[cluster_name]])
+  
+}
+
 cluster_validity_stats <- function(clustered_sce, cluster_name) {
-  # Purpose: Check the cluster purity of the clusters in the clustered SingleCellExperiment object
+  # Purpose: Check the cluster purity of the clusters in the clustered
+  # SingleCellExperiment object
+  
+  # Args:
+  #   clustered_sce: SingleCellExperiment object with clustered results
+  #   cluster_name: name associated with where the cluster results in the
+  #                 SingleCellExperiment object are stored
   
   # isolate the clusters stored in the `cluster_name` slot of the SCE object
   clusters <- clustered_sce[[(cluster_name)]]
@@ -100,8 +185,18 @@ cluster_validity_stats <- function(clustered_sce, cluster_name) {
   return(cluster_stats_df)
 }
 
-plot_clustering_validity <- function(cluster_validity_df, measure, colour_var, facet_var) {
-  # Purpose: Plot the relevant clustering data frame
+plot_clustering_validity <- function(cluster_validity_df, measure, colour_var,
+                                     facet_var) {
+  # Purpose: Plot the provided clustering data frame
+  
+  # Args:
+  #   clustered_validity_df: data frame with cluster validity stats
+  #   measure: string associated with the column whose values should be on the 
+  #            y-axis
+  #   colour_var: string associated with the column whose values should be used
+  #               color the points on the plot
+  #   facet_var: string associated with the column whose values should be used
+  #              to facet on, to create individual plots
   
   # convert into symbols for plotting
   measure <- rlang::sym(measure)
@@ -117,6 +212,11 @@ plot_clustering_validity <- function(cluster_validity_df, measure, colour_var, f
 plot_cluster_stability <- function(normalized_sce, cluster_name) {
   # Purpose: Plot the cluster bootstrapping stability values of the clusters
   # stored in the SingleCellExperiment object
+  
+  # Args:
+  #   normalized_sce: normalized SingleCellExperiment object
+  #   cluster_name: name associated with where the cluster results in the
+  #                 SingleCellExperiment object are stored
   
   # set the name from which the bootstrapping results can be retrieved from the SCE object
   bootstrapping_name <- paste("bootstrapping_results", cluster_name, sep = "_")

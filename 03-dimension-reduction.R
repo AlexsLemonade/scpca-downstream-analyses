5b1ea667ce6abfb6c6fb5c1e60773ced3bd76652
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-06-22 16:03:52 -0500

    Revert "Revert "pass project root to all Rscripts""
    This reverts commit ce09cad474d373e8260cc8165a25eceae5c06748.

--- 03-dimension-reduction.R
+++ 03-dimension-reduction.R
@@ -12,16 +12,6 @@
 #   --overwrite "yes"
 
 ## Set up -------------------------------------------------------------
-
-# define project root 
-project_root <- rprojroot::find_root(rprojroot::has_file("scpca-downstream-analyses.Rproj"))
-
-# Source in set up function
-source(file.path(project_root, "utils", "setup-functions.R"))
-
-# Load project
-setup_renv(project_filepath = project_root)
-
 # Check that R version us at least 4.1
 if (! (R.version$major == 4 && R.version$minor >= 1)){
   stop("R version must be at least 4.1")
@@ -70,12 +60,30 @@ option_list <- list(
     type = "character",
     default = NULL,
     help = "path to output dimensionality reduced RDS file"
+  ),
+  optparse::make_option(
+    c("--project_root"),
+    type = "character",
+    help = "the path to the root directory for the R project and where the `utils` folder lives."
   )
 )
 
 # Read the arguments passed
 opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
+
+# if project root is not provided use here::here()
+if(is.null(opt$project_root)){
+  project_root <- here::here()
+} else {
+  project_root <- opt$project_root
+}
+
+# Source in set up function
+source(file.path(project_root, "utils", "setup-functions.R"))
+
+# Load project
+setup_renv(project_filepath = project_root)
 
 # Check that the input file exists
 if (!file.exists(opt$sce)){

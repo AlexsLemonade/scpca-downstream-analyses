04657a9d40ee3ffaef0ef0fad966a2dfdcb9f6ab
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-07-21 14:11:07 -0400

    Add error check/messages for M1 conda install

--- envs/scpca-renv.post-deploy.sh
+++ envs/scpca-renv.post-deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-# Record CONDA_SUBDIR if it is set to specify Intel on an Apple Silicon mac
-# Used for compatibility with Bioconductor packages that may not work with Apple Silicon
-if [[ $CONDA_SUBDIR == 'osx-64' ]]; then
-  conda env config vars set CONDA_SUBDIR=osx-64
+# Check if we are on an Apple Silicon mac
+if [[ "$(uname)" == 'Darwin' && "$(uname -m)" == 'arm64' ]]; then
+  if [[ $CONDA_SUBDIR == 'osx-64' ]]; then
+    # Record CONDA_SUBDIR if it is set
+    conda env config vars set CONDA_SUBDIR=osx-64
+  else
+    # Error if CONDA_SUBDIR was not set
+    echo 'BioConductor is not compatible with arm64, rerun with `CONDA_SUBDIR=osx-64`' >&2
+    echo ' e.g. `CONDA_SUBDIR=osx-64 snakemake --use-conda --conda-create-envs-only -c1`' >&2
+    exit 1
+  fi
 fi
+
+
+
 
 # restore packages
 Rscript -e 'renv::restore()'

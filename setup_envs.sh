0287d7f5893d66bf6a67bb203679bb7f01c02714
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-08-23 11:53:40 -0400

    Fix extra slash

--- setup_envs.sh
+++ setup_envs.sh
@@ -11,7 +11,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # If on OSX with Apple Silicon, build the Intel version of R
 if [[ "$(uname)" == 'Darwin' && "$(uname -m)" == 'arm64' ]]; then
   echo "On Apple Silicon: Building R environment and required R packages with CONDA_SUBDIR=osx-64 for compatibility"
-  CONDA_SUBDIR=osx-64 snakemake --use-conda --conda-create-envs-only -c1 -R build_renv
+  CONDA_SUBDIR=osx-64 snakemake --use-conda --conda-create-envs-only -c1 build_renv
 fi
 
 echo "Building conda environments"

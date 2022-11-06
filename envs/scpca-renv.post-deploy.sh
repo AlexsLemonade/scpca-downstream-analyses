9e971a0c54752e20dd2e9f6e70c029bd7b56dab3
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-11-06 14:02:00 -0500

    update snakemake conda env

--- envs/scpca-renv.post-deploy.sh
+++ envs/scpca-renv.post-deploy.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# Check if we are on an Apple Silicon mac
-if [[ "$(uname)" == 'Darwin' && "$(uname -m)" == 'arm64' ]]; then
-  if [[ $CONDA_SUBDIR == 'osx-64' ]]; then
-    # Record CONDA_SUBDIR if it is set
-    conda env config vars set CONDA_SUBDIR=osx-64
-  else
-    # Error if CONDA_SUBDIR was not set
-    echo 'Bioconductor is not compatible with arm64, rerun with CONDA_SUBDIR=osx-64' >&2
-    echo ' e.g.: CONDA_SUBDIR=osx-64 snakemake --use-conda --conda-create-envs-only -c1 build_renv' >&2
-    exit 1
-  fi
-fi
-
-
-
 # install packages required for renv
 Rscript --vanilla -e \
   "

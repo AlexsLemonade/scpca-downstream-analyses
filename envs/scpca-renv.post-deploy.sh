d0611c858f762bfcaac6613704f28839ab3802fd
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2023-02-23 14:53:08 +0000

    Install tidyverse with conda & load glue

--- envs/scpca-renv.post-deploy.sh
+++ envs/scpca-renv.post-deploy.sh
@@ -3,8 +3,7 @@
 # install packages required for renv
 Rscript --vanilla -e \
   "
-   .libPaths(.Library)
-   install.packages(c('jsonlite', 'purrr'), repos = 'https://cloud.r-project.org')
+   library(glue)
    library(jsonlite)
    library(purrr)
    source('.Rprofile')

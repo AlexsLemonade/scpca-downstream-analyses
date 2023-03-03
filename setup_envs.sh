692dca9ce0017e56ed84c68e42aca906dc623fad
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2023-03-03 13:38:09 -0500

    Add back Apple Silicon checks

--- setup_envs.sh
+++ setup_envs.sh
@@ -7,5 +7,11 @@ set -euo pipefail
 # Run from the script file location
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+# If on OSX with Apple Silicon, build the Intel version of R
+if [[ "$(uname)" == 'Darwin' && "$(uname -m)" == 'arm64' ]]; then
+  echo "On Apple Silicon: Building R environment and required R packages with CONDA_SUBDIR=osx-64 for compatibility"
+  CONDA_SUBDIR=osx-64 snakemake --use-conda --conda-create-envs-only -c1 -R build_renv
+fi
+
 echo "Building conda environments"
 snakemake --use-conda --conda-create-envs-only -c1

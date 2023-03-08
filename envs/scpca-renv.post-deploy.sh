b2e4ab9e2ef05ad1bb72ae40fdfa3abf2bcfde84
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2023-03-08 20:28:15 +0000

    Deactivate R_LIBS_USER within conda environments

--- envs/scpca-renv.post-deploy.sh
+++ envs/scpca-renv.post-deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+# Deactivate system set user library
+R_LIBS_USER='' 
+# and make the change permanent
+conda env config vars set R_LIBS_USER=''
+
 # set conda subdir if on Apple Silicon
 if [[ "$(uname)" == 'Darwin' && "$(uname -m)" == 'arm64' ]]; then
   conda config --env --set subdir osx-64
 fi
 
-SCPCATOOLS_VERS="v0.2.1"
 
-# remove any settings for R_LIBS_USER that might have crept in (renv/RStudio?)
-R_LIBS_USER="" 
+SCPCATOOLS_VERS='v0.2.1'
 
 # install github packages
 Rscript --vanilla -e \

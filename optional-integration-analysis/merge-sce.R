3e39f32a97f34f2a36b1f66da8f1ae301f2f80f4
cbethell <chantebethell@gmail.com>
2023-05-12 14:02:28 -0500

    add dimension reduction results to merge SCE script

--- optional-integration-analysis/merge-sce.R
+++ optional-integration-analysis/merge-sce.R
@@ -105,5 +105,37 @@ merged_sce <- scpcaTools::merge_sce_list(sce_list,
                                          preserve_rowdata_cols = "gene_symbol",
                                          cell_id_column = "cell_id")
 
+# HVG selection ----------------------------------------------------------------
+
+# Extract the batch column
+batch_column <- merged_sce$library_id
+
+# Model gene variance
+gene_var_block <- scran::modelGeneVar(merged_sce,
+                                      block = batch_column,
+                                      BPPARAM = bp_param)
+# Identify subset of variable genes
+hvg_list <- scran::getTopHVGs(gene_var_block,
+                              n = opt$n_hvg)
+
+metadata(merged_sce)$merged_hvgs <- hvg_list
+
+# Dim Reduction PCA and UMAP----------------------------------------------------
+
+# Multi batch PCA on merged object
+multi_pca <- batchelor::multiBatchPCA(merged_sce,
+                                      subset.row = hvg_list,
+                                      batch = batch_column,
+                                      preserve.single = TRUE,
+                                      BPPARAM = bp_param)
+
+# Add PCA results
+reducedDim(merged_sce, "PCA") <- multi_pca[[1]]
+
+# Add UMAP results
+merged_sce <- scater::runUMAP(merged_sce,
+                              dimred = "PCA",
+                              BPPARAM = bp_param)
+
 # Save combined SCE object
 readr::write_rds(merged_sce, opt$output_sce_file)

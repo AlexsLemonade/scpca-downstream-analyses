2e90261d1138518ee36be792e91de4149d059afb
cbethell <chantebethell@gmail.com>
2023-06-06 09:51:24 -0500

    implement review suggestions
    - change `merged_sce_rds` -> `merged_sce_file`
    - add necessary checks
    - add integrated UMAPs to object

--- optional-integration-analysis/perform-integration.R
+++ optional-integration-analysis/perform-integration.R
@@ -12,16 +12,16 @@ library(scpcaTools)
 # Declare command line options
 option_list <- list(
   make_option(
-    opt_str = c("--merged_sce_rds"),
+    opt_str = c("--merged_sce_file"),
     type = "character",
     help = "Path to the RDS file containing the merged SCE object"
   ),
   make_option(
     opt_str = c("--integration_method"),
     type = "character",
-    default = c("fastMNN", "harmony"),
+    default = "fastMNN, harmony",
     help = "The integration method(s) to use when performing integration; 
-    default is c('fastMNN', 'harmony')"
+    default is 'fastMNN, harmony'"
   ),
   make_option(
     opt_str = c("-o", "--output_sce_file"),
@@ -60,26 +60,50 @@ if(!(stringr::str_ends(opt$output_sce_file, ".rds"))){
   stop("output file name must end in .rds")
 }
 
+# Check that merged SCE file exists
+if(!file.exists(opt$merged_sce_file)){
+  stop(paste(opt$merged_sce_file, "does not exist."))
+}
+
 # Read in merged SCE -----------------------------------------------------------
 
-merged_sce <- readr::read_rds(opt$merged_sce_rds)
+merged_sce <- readr::read_rds(opt$merged_sce_file)
 
 # Integrate SCEs ---------------------------------------------------------------
 
+# Split up string of integration methods
+integration_methods <-  stringr::str_split(opt$integration_method, ",") %>%
+  unlist() %>%
+  stringr::str_trim()
+
+# Check that we support the provided integration method(s)
+for (integration_method in integration_methods) {
+  if (!integration_method %in% c("fastMNN", "harmony")) {
+    stop("--integration_method must be either fastMNN, harmony, or both (comma-separated)")
+  }
+}
+
+# Check that `library_id` is in merged SCE object
+if(!("library_id" %in% colnames(colData(merged_sce)))){
+  stop("The 'library_id' column is missing from the `colData` of the merged SCE 
+       object and is needed for integration.")
+}
+
 # Perform integration with specified method
-if ("fastMNN" %in% opt$integration_method) {
+if ("fastMNN" %in% integration_methods) {
   integrated_sce <- integrate_sces(merged_sce,
                                    integration_method = "fastMNN",
                                    batch_column = "library_id")
+  scater::runUMAP(integrated_sce, dimred = "fastMNN_PCA", name = "fastMNN_UMAP")
 }
 
-if ("harmony" %in% opt$integration_method) {
+if ("harmony" %in% integration_methods) {
   integrated_sce <- integrate_sces(merged_sce,
                                    integration_method = "harmony",
                                    batch_column = "library_id")
+  scater::runUMAP(integrated_sce, dimred = "harmony_PCA", name = "harmony_UMAP")
 }
 
 # Write integrated object to file ----------------------------------------------
 
-write_rds(integrated_sce, opt$output_sce_file)
-
+readr::write_rds(integrated_sce, opt$output_sce_file)
